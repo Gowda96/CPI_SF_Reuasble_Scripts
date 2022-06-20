33eef135b4176adec366b6ecbf8e48caedc4815d
Gowda96 <varungowdak86@gmail.com>
2022-06-20 11:23:32 +0530

    Add files via upload
    Added OData_Filter_Creation groovy

---
+++ GV_Create_Odata_Filter_condition.groovy
@@ -0,0 +1,253 @@
+/*
+
+Link to Groovy CPI editor.
+
+https://groovyide.com/cpi/share/v1/xVgLb9s2EP4rHBEkTq34kSZ7uPOGLHW2AE5axM6AYhoCRqIcrbLkSVTaIPV_3x0pUZREJ26xbm7hyOS9ePfdd7QfaRivckFHj_Q28R_oiFKH3nHm8zSDxbVDV2my4qkIOX6m1xlPb859eKZHw---Pzo-AvnTJI9FisrXsxP4PFmuouSB85uZYCJH2RPnLaxfsDhn0c1VHsPSAxh06K_JzTS85zevmeCweDg4PDwYHB8MhvPBYCT_g9B0dvVab357MDyeD4ejo2KzMPq0TOKj9SCPIjO804hlGN0FLgYB9wSGcsW9JJUnDHKRpxwD4AtwMYlFKPCUx4PB0IG3l_B2eETXkKXMS8OVTKOnfIXLVZIK4iXLXsZWvQWc7wN76IWrHljnPS_PRLL0-X0vF2HUu-BZxhb8lRsXen-xe6a2IIfee9iwbE3DTMCOzwNSGCBQLQ8eIZ2sU64t1d998ohWCLxQo6rruBDoLbh4q1c7-5VsDlWHoo8rHZTtuBoOLjWkPQWHtnSBk5owh2IomLTlGzhq6cn6PaEm92taSwkWAGBbqwJnTWORID4Rnm0VE7w1pShL_bY4QtQSzdQqbMC6fu6P3IPiJzFi2qIGqzV5gYmC-M4tPubFHlQQdAgxvJTdoJrBkuNmuzQMRNgxqmEsiTDaydCrtIMwZtFZGAmeniaxH-J5xy51aV2ovW8EwDIByQiDkPubZDLBUoHFe1bgLGKLsUhzLvdwi3h5mvJYqOPL_YBFGTdb7IKtfpyJNIwXjvrzE_LBMom1v0wd8pIt-e8syjkh4z-UfvlyadFOb4LTZLliMWRsVCw5bdFSwsi_EvrzmcCQGxE8Twbk0qpdL9l9n38EWSjVqUQdGRndvJ1Xn0eCWdy-ekZP9_HT-XOpYi5ISEFhlrBMpChzGRih5jJCYB4u4YyNlFdJSR6WgAZMil1Ru-z3z5KUnMnRQn4B9AD7Vv1GZh6PWRomNhDq4IyC6N1abM2YoP-yJK6eWHQeBwl-NvSN3Lhxv3_i-6RwST7chd4dYRCxAjAJ4Awsiio6gsw-g-0eZ97do3JibXDbYjeM30DIDJ87oejdoyUHHt7zB8emANS3Lk8w44IggZLE4E0VQBh0JEmPY8A9-fRJUfZ4b6-gzscql3KnGgPVRjk2Mz02HzTLO6hV2FqraKpMx7WyjlG0l-W3mQR3Z-AMB6Cp9xE_Y_2pC7RdXo2wZFWtmK8ywKKCGjNZJdUqJM2lKJxb905PJNcrCPuUZbyzD4d_N5nh-R81Gcu3Tb1m1vOr11TmkejaVZOTfKNKuLtLaou6ko1q6opW0vXNbataVLZ85JL7Nzjs9y8ThUSsiBrtZFJH5HYB1I4lXykHJolLrVZsuh3Kf7q4pFzBRomAfoge6qQc6rCbfQiFd9dp3Ajq2EHQKJsefAb2Ob2-uppczoF3jWibRNYEkGUct5e6t3kY-bO6KUCR0xZ1ai1UNWM9FmNyywO0xroe--Q25ey9Hk3FUX87n83fXL2rHdV6sdgViWz3nRblmrYNy2fX8-uryRaGgzRZfp7pkyjaPuDh4cHL4cEP8LJZbcBMcdEUZiBZFkOwoCOJfTnwTTKGZq5daVuk9HoynZ_sVQhT75ah3YST_QZoXVWgmrZsIq6sCk16F_gIR2nnUVIRslMbWBJZ0oj8YmaP136Mn9vXEPIF5yYwTQhUxaXdduSjzT7awiir-LlimU2Xu_9-cLjV1Lc6k9f-9uTfPjDI4K7r7iigj4GuSXdDIbroiCoUF1xUjY4v9U1Y7BPp1KXgvLsJBcq5QYS6bXnx1eXZdjy7nk6NbtzwteF_K3H_BRa51Wvt2lZPG0Ah-3b_uTO4dMe2LgvS2Wn3iU7_1neHDW5NuD3pp3K1ftE3yBqdIu8I-31jkqZJiheOdzzDertCXTQ0Yky2K1L82Zd7G49I8_aYlPpNJUw3QaJ5KdJ3ILx5qy-RxHqRKPZiAHLxZROJ25wM5bKOff_RoJeG8CZmaYqhP9m9hP9NfDAq4MayZ9IytO4e_v5BmhB-ikSabkhJ9-BLudzeY5HCtS2Llsm5fSLlKP1Xc7gQ-kRJEACG8Fzo5uslcVuX6GjdgqJJesWSIr7iA_iN_Mt6KlWeq8ShOYC9pF_jZ6wwIB1p6-ceqC7hrl6eTa6O5Xsv5auIeRyuhR38HYE6cLw9Z0-nvaqMcluryQbiwpcOqD4gITf6SKpkYUwwYyooopJWN7euf6xz5hbuixnZJV_iuVoomEWbfh4_hYZVAfqJOjTIYw9rT0fU-P2ertf_AA*/
+
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.Stack;
+
+import java.util.List;
+def Message processData(Message message) {
+
+    def properties=message.getProperties()
+    def userIds=properties.get("User_Ids")
+    def country=properties.get("Country")
+    def emplStatus=properties.get("Employee_Status")
+    def emplClass=properties.get("Employee_Class")
+    def manualRun=properties.get("Manual_Run")
+    def goLiveDate=properties.get("Go_Live_Date")
+    def lsrd=properties.get("LSRD")
+    def manualLsrd=properties.get("Manual_LSRD")
+    def executionMode=properties.get("Mode")
+    def templateId=properties.get("Template_Id")  
+    def effectiveRecord=properties.get("Effective_Records")  
+    def legalEntity=properties.get("Legal_Entity")  
+    
+    def finalFilterCondition=""  
+    def filterCondition=""
+    def lastModifiedCondition=""
+    def startDateCondition=""
+    def startDateFlag=true
+    def  currentRecordFlag=false
+
+	
+		
+    def Map<String,String> commonConditionsFilterNameValue  =[
+            "countryOfCompany":country,
+            "company":legalEntity
+    ]
+
+    def Map<String,String> fullModeFilterNameValue  =[
+        "emplStatusNav/externalCode" : emplStatus
+    ]
+
+    def Map<String,String> deltaModeFilterNameValue;
+
+    def Map<String,String> manualRunFilterNameValue  =[
+         "userId":userIds
+    ]
+
+    def lastModifiedFilters=["lastModifiedDateTime",
+                "employmentNav/lastModifiedDateTime"]
+
+    //For Future Becomes effective Scenario
+    def startDateFilters=[
+        "startDate",
+        "employmentNav/personNav/personalInfoNav/startDate"
+    ]
+
+
+//Add Filters which are common for all executions
+commonConditionsFilterNameValue.each{
+    finalFilterCondition=finalFilterCondition+inOperation(it.value,it.key,finalFilterCondition)
+}
+
+
+//Set LSRD of execution
+    if(lsrd==null || lsrd=='')
+    {
+        lsrd=goLiveDate
+        message.setProperty("LSRD",lsrd)
+    }
+
+//startDate
+def startDate=lsrd.substring(0,10)
+startDateTime=startDate+"T00:00:00"
+
+
+//Add additional filters for manual run
+
+if(manualRun.toUpperCase()=='YES')
+{    
+   
+   manualRunFilterNameValue.each{
+        finalFilterCondition=finalFilterCondition+inOperation(it.value,it.key,finalFilterCondition)
+    } 
+	
+    if(manualLsrd !=null && manualLsrd !='')
+        {
+            lsrd=manualLsrd
+            message.setProperty("LSRD",lsrd)
+        }
+        else
+        {
+            //No LSRD for Manual Execution
+             message.setProperty("LSRD",'')
+             return message
+        }
+    
+}
+
+
+
+
+//Select Effective Records
+
+switch(effectiveRecord.toUpperCase())
+{
+    case "CURRENT":
+         startDateFilters.each{
+        startDateCondition=startDateCondition+buildStartDateFilter(it,startDateCondition,startDateTime)
+    }
+    startDateFlag=false
+    currentRecordFlag=true
+    break;
+
+    case "HISTORY":
+        startDateCondition="&toDate=$startDate"
+        break;
+    case "FUTURE":
+        startDateCondition="&fromDate=$startDate"
+        break;
+    case "All":
+        startDateCondition="&toDate=12-31-9999"
+        break;
+
+    
+}
+
+
+//Add Last modified filter for delta execution
+if(executionMode.toUpperCase()=='DELTA')
+{
+    
+    lastModifiedFilters.each{
+        lastModifiedCondition=lastModifiedCondition+buildLastModifiedFilter(it,lastModifiedCondition,lsrd)
+    }
+	
+  if(startDateCondition !='' && currentRecordFlag==true)
+  {
+
+lastModifiedCondition= lastModifiedCondition?
+                         lastModifiedCondition=lastModifiedCondition +" or "+startDateCondition:
+                        startDateCondition
+  } 
+  
+
+
+    deltaModeFilterNameValue.each{
+        finalFilterCondition=finalFilterCondition+inOperation(it.value,it.key,finalFilterCondition)
+    }
+
+    if(finalFilterCondition=="")
+    {
+        finalFilterCondition=finalFilterCondition+ "&\$filter=(" +  lastModifiedCondition+")" 
+   }
+    else
+    {
+        finalFilterCondition=finalFilterCondition+ " and " + "("+   lastModifiedCondition+")"
+    }
+  
+}
+
+
+else
+
+if(executionMode.toUpperCase()=='FULL')
+{
+    fullModeFilterNameValue.each{
+        finalFilterCondition=finalFilterCondition+inOperation(it.value,it.key,finalFilterCondition)
+    }
+
+   /* if(currentRecordFlag)
+    {
+        
+        if(finalFilterCondition!='' ){
+        finalFilterCondition="$finalFilterCondition and ($startDateCondition)"
+     }
+        else
+        {
+            finalFilterCondition="&\$filter=($startDateCondition)"
+        }
+    }*/
+    
+}
+else{
+	message.setProperty("Error",'Yes')
+	return
+}
+
+
+if(startDateFlag)
+{
+    finalFilterCondition=finalFilterCondition+startDateCondition
+}
+
+message.setProperty("Filter_Condition",finalFilterCondition)
+return message
+}
+
+
+
+def String buildStartDateFilter(String name,String curLastModified,String startDate){
+
+    if(curLastModified=="")
+    {
+        curLastModified=name + " eq datetime'"+startDate+"'"   
+        
+    }
+    else
+    {
+        curLastModified= " or " + name +" eq datetime'"+startDate+"'"   
+    }
+
+
+}
+
+def String buildLastModifiedFilter(String name,String curLastModified,String lsrd){
+
+    if(curLastModified=="")
+    {
+        curLastModified=name + " gt datetimeoffset'"+lsrd+"'"   
+        
+    }
+    else
+    {
+        curLastModified= " or " + name +" gt datetimeoffset'"+lsrd+"'"   
+    }
+ }
+
+
+def String inOperation(String value,String fieldName,String curFilter){
+
+    def retValue=""
+    if (value?.trim()) {
+    value=value.replaceAll(",", "','")
+        if(curFilter=="")
+            {
+                retValue= "&\$filter="+fieldName + " in '"+ value +"'"
+            }
+            else
+            {
+                retValue= " and "+ fieldName + " in '"+ value +"'"
+            }
+
+        return  retValue
+    }
+    else
+    {
+        return retValue
+    }
+    
+}
