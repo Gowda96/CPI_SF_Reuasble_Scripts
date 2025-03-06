d7ec267e59e423a0f0969eaeac41c3fcbe8aae45
Gowda96 <varungowdak86@gmail.com>
2025-03-06 17:20:46 +0530

    Update GV_Create_Odata_Filter_condition.groovy

--- GV_Create_Odata_Filter_condition.groovy
+++ GV_Create_Odata_Filter_condition.groovy
@@ -2,8 +2,7 @@
 
 Link to Groovy CPI editor.
 
-https://groovyide.com/cpi/share/v1/xVgLb9s2EP4rHBEkTq34kSZ7uPOGLHW2AE5axM6AYhoCRqIcrbLkSVTaIPV_3x0pUZREJ26xbm7hyOS9ePfdd7QfaRivckFHj_Q28R_oiFKH3nHm8zSDxbVDV2my4qkIOX6m1xlPb859eKZHw---Pzo-AvnTJI9FisrXsxP4PFmuouSB85uZYCJH2RPnLaxfsDhn0c1VHsPSAxh06K_JzTS85zevmeCweDg4PDwYHB8MhvPBYCT_g9B0dvVab357MDyeD4ejo2KzMPq0TOKj9SCPIjO804hlGN0FLgYB9wSGcsW9JJUnDHKRpxwD4AtwMYlFKPCUx4PB0IG3l_B2eETXkKXMS8OVTKOnfIXLVZIK4iXLXsZWvQWc7wN76IWrHljnPS_PRLL0-X0vF2HUu-BZxhb8lRsXen-xe6a2IIfee9iwbE3DTMCOzwNSGCBQLQ8eIZ2sU64t1d998ohWCLxQo6rruBDoLbh4q1c7-5VsDlWHoo8rHZTtuBoOLjWkPQWHtnSBk5owh2IomLTlGzhq6cn6PaEm92taSwkWAGBbqwJnTWORID4Rnm0VE7w1pShL_bY4QtQSzdQqbMC6fu6P3IPiJzFi2qIGqzV5gYmC-M4tPubFHlQQdAgxvJTdoJrBkuNmuzQMRNgxqmEsiTDaydCrtIMwZtFZGAmeniaxH-J5xy51aV2ovW8EwDIByQiDkPubZDLBUoHFe1bgLGKLsUhzLvdwi3h5mvJYqOPL_YBFGTdb7IKtfpyJNIwXjvrzE_LBMom1v0wd8pIt-e8syjkh4z-UfvlyadFOb4LTZLliMWRsVCw5bdFSwsi_EvrzmcCQGxE8Twbk0qpdL9l9n38EWSjVqUQdGRndvJ1Xn0eCWdy-ekZP9_HT-XOpYi5ISEFhlrBMpChzGRih5jJCYB4u4YyNlFdJSR6WgAZMil1Ru-z3z5KUnMnRQn4B9AD7Vv1GZh6PWRomNhDq4IyC6N1abM2YoP-yJK6eWHQeBwl-NvSN3Lhxv3_i-6RwST7chd4dYRCxAjAJ4Awsiio6gsw-g-0eZ97do3JibXDbYjeM30DIDJ87oejdoyUHHt7zB8emANS3Lk8w44IggZLE4E0VQBh0JEmPY8A9-fRJUfZ4b6-gzscql3KnGgPVRjk2Mz02HzTLO6hV2FqraKpMx7WyjlG0l-W3mQR3Z-AMB6Cp9xE_Y_2pC7RdXo2wZFWtmK8ywKKCGjNZJdUqJM2lKJxb905PJNcrCPuUZbyzD4d_N5nh-R81Gcu3Tb1m1vOr11TmkejaVZOTfKNKuLtLaou6ko1q6opW0vXNbataVLZ85JL7Nzjs9y8ThUSsiBrtZFJH5HYB1I4lXykHJolLrVZsuh3Kf7q4pFzBRomAfoge6qQc6rCbfQiFd9dp3Ajq2EHQKJsefAb2Ob2-uppczoF3jWibRNYEkGUct5e6t3kY-bO6KUCR0xZ1ai1UNWM9FmNyywO0xroe--Q25ey9Hk3FUX87n83fXL2rHdV6sdgViWz3nRblmrYNy2fX8-uryRaGgzRZfp7pkyjaPuDh4cHL4cEP8LJZbcBMcdEUZiBZFkOwoCOJfTnwTTKGZq5daVuk9HoynZ_sVQhT75ah3YST_QZoXVWgmrZsIq6sCk16F_gIR2nnUVIRslMbWBJZ0oj8YmaP136Mn9vXEPIF5yYwTQhUxaXdduSjzT7awiir-LlimU2Xu_9-cLjV1Lc6k9f-9uTfPjDI4K7r7iigj4GuSXdDIbroiCoUF1xUjY4v9U1Y7BPp1KXgvLsJBcq5QYS6bXnx1eXZdjy7nk6NbtzwteF_K3H_BRa51Wvt2lZPG0Ah-3b_uTO4dMe2LgvS2Wn3iU7_1neHDW5NuD3pp3K1ftE3yBqdIu8I-31jkqZJiheOdzzDertCXTQ0Yky2K1L82Zd7G49I8_aYlPpNJUw3QaJ5KdJ3ILx5qy-RxHqRKPZiAHLxZROJ25wM5bKOff_RoJeG8CZmaYqhP9m9hP9NfDAq4MayZ9IytO4e_v5BmhB-ikSabkhJ9-BLudzeY5HCtS2Llsm5fSLlKP1Xc7gQ-kRJEACG8Fzo5uslcVuX6GjdgqJJesWSIr7iA_iN_Mt6KlWeq8ShOYC9pF_jZ6wwIB1p6-ceqC7hrl6eTa6O5Xsv5auIeRyuhR38HYE6cLw9Z0-nvaqMcluryQbiwpcOqD4gITf6SKpkYUwwYyooopJWN7euf6xz5hbuixnZJV_iuVoomEWbfh4_hYZVAfqJOjTIYw9rT0fU-P2ertf_AA*/
-
+https://groovyide.com/cpi/share/v1/xVhtb9s2EP4rnBDETq3ISrJ-mDuvyFJnC-Ckhe0MKKYhYCTK0aoXV6LSBq7_-44voiiJjt1i3dzCkcl7491zz9FeW1G6Kqk1Wlv3WfBkjSzLth4IDkhewOLGtlZ5tiI5jQj7bN0WJL-7CuDZct1T1z07_REULrIypbnUniSrOHsi5G5OMS2Z5Ln9DtavcVri-G5WprCUZrDyW3Y3jR7J3RtMCayduqcvj92zY_flwnVH_D8ITeezN2LTPXZPjt1TfVPabMucNWSygFkPSEyxHt5FjAsW3TVbDEPiUxbLjPhZzs93cTubTW4WLASyBCeTlEaUn3EDaSn8PFrxvPnCfpSsspwiP0ucAq-cJZzpE35yopUDBonjlwXNkoA8OiWNYueaFAVekldeKvX-xo9YbEHa_A-wYdiaRgWFnYCESBpAUB4fHiGFuF-tJeLvEVozKwheTKMu5FgKOEtC36nV_lEtW0KZocrjWofJ9j1Vf8_SpH1R_q60xEVDmED-BTK68i3odPR4yZ5R4_sNrYQDBDDX1arx2NBYZgyTDJJdFR2wDaW4yIOuOIOlIZqpUViDcvPcn4kPxc9ShmODGqw25ClLFMR3ZfCxkHtQQdBBSPNSNYDAvyHH7Q5pGYhZj4gWMSRCayBNr9YOoxTHl1FMSX6RpUHEzjv2LM9qCnX3tQBwQSEZURiRYJtMQXFOWfF2ClzGeDmmeUn4HttCfpnnJKXi-Hw_xHFB9Ba7xquf5zSP0qUt_vzC-CDJUuWvEIe8wQn5A8clQWj8p9CvXp4l2-lteJElK5xCxkZyye6KVhJa_oXQXzsCC8s4ZuB5NiDPqtv1Bj8OyWeQhVJdcNShkdbN-3nlLGxw-2qHnurj5_PnWYK5ICGSwgxh6UgR5gowYunLDAKLKIEztlJeJyV7SgANLClmReVyOLzMcnRZ0jIn6FdAD7Bv3W9o7pMU51FmAqEKTiuI2m3E1o4J-q_I0voJx1dpmLHPmr6WGy8dDs-DAEmX6NND5D8gDBELAKMQzoDjuKYjyOwObDsE-w9r4cTY4KbFQZS-hZAxe-5H1Hlklmx4-ECebJMCUN-mOsGcUMQIFGUab4oAorDPSXqcAu7Rly-Csse9nqTOdZ1LvlOPgXqjGpuFGptPiuVtpiVtyXjqrOJAxIpjSWIFz6cANcpLLgoRKpQ7NLtdgYMLXJD-EYT5fjJnka4VbfK3bV2hZ_67Z5-fGKks1zMO_SCSfXiIGosq5628q9zX0s3NffMva1A9Es7SWxwOhzeZwAyriBjCaNLEzn4BNI7FXzmBnk8rrU5sErhDrSXTRv-P2ZmcorwvOAv2XfvEBR9qnxHNWH0awHyvrr2st6t_CjKoBuWcxEA_SA11VA112C0-RdR_6LduBE1EMigKmz58BvaRl2XgXS0HbSJrw9IwjrtLg_syioN50xRg0-6K2o3M1M3YjEWb3PwAnbGuxj66zwn-oEaTPOrvV_PF29n7xlGNF4tDmvEqHnQoV7etWb68XdzOJnsYDvMs-TrT59Pp_gH_BK_jk9PjsxOT1QZ4K4abwgxEiRyCkuR4R_GBr5MxUETjStuhujeT6eK8VyNMvBuGdhtO5hugcVWAatqxyXBlVGjTO2WPcJRuHjnBMc7rAosjixvhX8zM8ZqP8bp7DUHfcG4EJIGgKp416EY-2u6jK8xkBevXLLPtcvffjyOvnvpGZ_za3538-wcGGTz0vAMB9DEMATTYUogBc2QJFEsuqgfSt_pGOA0Qd-pZ4HywDQXCuUaEqm2J_Oqysx0vb6dTrRu3fG3430o8fMGK3Om1bm3rpy2g4H17tOsMnnVgWucF6R90-0Slf-8byRa3Otye9VO72rwYamTNnDLeoeZbzCTPs5xdY96TgtXbo-L6ohCjs51M8Vdf7k08ws2bYxLqd7WwtQ0S7auWuoKzC5X4EomMFwm5lwKQ5ZdNRtz6ZKiWVexHa41eWsLbmKUtxvzx7kXkIwrAKIUbS0-nZWjdHvv9A7Uh_ByJtN2giu7Bl3C5v0eZwo0pi4bJuX8i-Sj9V3O4pOpEWRgChti5mJvvl8R9XTJHmw4UddKTS4L45AfwGwc3zVSKPNeJY-YA9px-tZ-xohD1ua3XDqgmcFevzsZXx_zdyckqxj45j-M--x3BsuF4Pbun0l5XRrht1GQLcbGXCqg5ICE36kiiZFGKWMZEUEgkrWlu0_zY5Mw93MsZOUDf4rlekMyiTO_Gj9QwKkA_WbYVlqnPam-NLO33e2uz-Qc*/
 
 
 import com.sap.gateway.ip.core.customdev.util.Message;
@@ -31,10 +30,8 @@ def Message processData(Message message) {
     def lastModifiedCondition=""
     def startDateCondition=""
     def startDateFlag=true
-    def  currentRecordFlag=false
+   def  currentRecordFlag=false
 
-	
-		
     def Map<String,String> commonConditionsFilterNameValue  =[
             "countryOfCompany":country,
             "company":legalEntity
@@ -73,9 +70,7 @@ commonConditionsFilterNameValue.each{
         message.setProperty("LSRD",lsrd)
     }
 
-//startDate
-def startDate=lsrd.substring(0,10)
-startDateTime=startDate+"T00:00:00"
+
 
 
 //Add additional filters for manual run
@@ -86,7 +81,6 @@ if(manualRun.toUpperCase()=='YES')
    manualRunFilterNameValue.each{
         finalFilterCondition=finalFilterCondition+inOperation(it.value,it.key,finalFilterCondition)
     } 
-	
     if(manualLsrd !=null && manualLsrd !='')
         {
             lsrd=manualLsrd
@@ -101,6 +95,15 @@ if(manualRun.toUpperCase()=='YES')
     
 }
 
+//startDate
+def startDate=lsrd.substring(0,10)
+startDateTime=startDate+"T00:00:00"
+
+
+
+
+    
+    
 
 
 
@@ -122,8 +125,8 @@ switch(effectiveRecord.toUpperCase())
     case "FUTURE":
         startDateCondition="&fromDate=$startDate"
         break;
-    case "All":
-        startDateCondition="&toDate=12-31-9999"
+    case "ALL":
+        startDateCondition="&toDate=9999-12-31"
         break;
 
     
